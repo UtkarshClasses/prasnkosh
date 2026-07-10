c4a90da46cbf772809cea96cf4ae19cb98ed49b3
Chetan <parmar.chetan10@gmail.com>
2026-07-10 12:42:09 +0530

    add server api

--- lib/utility/global_variables.dart
+++ lib/utility/global_variables.dart
@@ -1,5 +1,5 @@
 // const nodeBaseURL =
 //     "https://0a74-2401-4900-1ca9-9623-14cd-292f-e91a-968d.ngrok-free.app/";
-const nodeBaseURL = "http://localhost:3005/";
+//const nodeBaseURL = "http://localhost:3005/";
 //const nodeBaseURL = "https://teachbackendapi.utkarshpublications.com/";
-// const developmentServer = "http://192.168.1.2:8080/";
+const nodeBaseURL = "http://3.7.91.190:3005/";
